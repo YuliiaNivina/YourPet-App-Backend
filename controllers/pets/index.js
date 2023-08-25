329030b86e1afd91ff457f11b8720d4a26fe756f
YuliiaNivina <112802335+YuliiaNivina@users.noreply.github.com>
2023-08-25 07:56:30 +0300

    Update index.js

--- controllers/pets/index.js
+++ controllers/pets/index.js
@@ -1,10 +1,8 @@
-const authentification = require('./authentification')
 const getUserPets = require('./getUserPets')
 const addUserPet = require('./addUserPet')
 const deleteUserPet = require('./deleteUserPet')
 
 module.exports = {
-    authentification,
     getUserPets,
     addUserPet,
     deleteUserPet,
