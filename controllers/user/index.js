11bb92c47a09c3677304630f96e33b8b5a3df51b
YuliiaNivina <112802335+YuliiaNivina@users.noreply.github.com>
2023-08-25 07:59:09 +0300

    Update index.js

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,10 +1,8 @@
-const authentification = require('./authentification')
 const getCurrent = require('./getCurrent') 
 const updateUserData = require('./updateUserData')
 const updateUserAvatar = require('./updateUserAvatar')
 
 module.exports = {
-    authentification,
     getCurrent,
     updateUserData,
     updateUserAvatar,
