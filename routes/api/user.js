34800ad29209897a4678c674d9a9d73cbddc988b
YuliiaNivina <112802335+YuliiaNivina@users.noreply.github.com>
2023-08-25 08:01:59 +0300

    Update user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,25 +2,25 @@ const express = require("express");
 
 const ctrl = require("../../controllers/user");
 
-const { upload, validateBody } = require("../../middlewares");
+const { upload, validateBodyб, authenticate } = require("../../middlewares");
 const {schemas} = require("../../models/user");
 
 const router = new express.Router();
 
 const { ctrlWrapper } = require("../../helpers");
 
-router.get("/current", ctrl.authentification, ctrlWrapper(ctrl.getCurrent));
+router.get("/current", authenticaten, ctrlWrapper(ctrl.getCurrent));
 
 router.patch(
   "/update",
-  ctrl.authentification,
+  authenticate,
   validateBody(schemas.joyUpdateSchema),
   ctrlWrapper(ctrl.updateUserData)
 );
 
 router.patch(
   "/update/avatar",
-  ctrl.authentification,
+  authenticate,
   upload.single("avatarURL"),
   ctrlWrapper(ctrl.updateUserAvatar)
 );
