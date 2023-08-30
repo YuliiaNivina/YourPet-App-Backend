73838459d4d534d4ac7fcd50823b27b92088baa2
YuliiaNivina <112802335+YuliiaNivina@users.noreply.github.com>
2023-08-30 14:38:38 +0300

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -2,8 +2,8 @@ const { isValidObjectId } = require("mongoose");
 const { ResultError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
-  const { _id } = req.params;
-  if (!isValidObjectId(_id)) {
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
 
     next(ResultError(404, "Invalid id"));
   }
